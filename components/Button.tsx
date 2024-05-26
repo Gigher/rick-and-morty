fef737802dbcad700e3c822f1f2005f48ce14b8d
Gigher <gastelo1301@gmail.com>
2024-05-26 21:40:10 +0300

    button font resolve

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,11 +1,14 @@
 import { ButtonProps } from '@/types';
 import React from 'react';
+import { Roboto } from 'next/font/google';
 
 import styles from "../styles/modules/Button.module.scss";
 
+const roboto = Roboto({ weight: ['500'], subsets: ["latin"] });
+
 const Button = ({ title }: ButtonProps) => {
   return (
-    <button className={styles.button}>
+    <button className={`${styles.button} ${roboto.className}`}>
         {title}
     </button>
   )
